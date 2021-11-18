11c702f9687e97071108da2fa9ca223b6017c5f7
Andrii Bondar <andy.bondar@gmail.com>
2021-11-18 22:32:25 +0200

    SSH public key injection

--- modules/compute/vm/variables.tf
+++ modules/compute/vm/variables.tf
@@ -3,3 +3,13 @@ variable "vm_properties" {
   description = "VM properties"
   default = {}
 }
+
+variable "ssh_user" {
+  type = string
+  description = "SSH user's name"
+}
+
+variable "ssh_pub_key_file" {
+  type = string
+  description = "Server's user SSH public key"
+}
